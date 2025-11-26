2973b661363b2bcdd87b09da5b49a9f9a45409df
Guilherme Basilio <gbasilio06@proton.me>
2025-11-26 15:32:13 -0300

    Projeto Spring

---
+++ src/main/java/com/senai/br/TechMec/TechMecApplication.java
@@ -0,0 +1,13 @@
+package com.senai.br.TechMec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechMecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/br/TechMec/TechMecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.br.TechMec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
